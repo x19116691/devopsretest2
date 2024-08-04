d3ac51c9a9b2f31e1ce07d50b276cab0249c2e7a
Conor-Corboy <58186008+x19116691@users.noreply.github.com>
2024-08-04 17:45:12 +0100

    U

--- .github/workflows/node.js
+++ .github/workflows/node.js
@@ -1,17 +1,1 @@
-// index.js
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('project dev ops');
-});
-
-// Add a new route
-app.get('/new-route', (req, res) => {
-  res.send('This is a new route!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
